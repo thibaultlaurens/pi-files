eb2af0327863f8d6117a6a7bf02984d37027255b
Thibault Laurens <laurens.thibault@gmail.com>
2020-05-04 18:37:37 +0200

    update iptables base rules

--- iptables.sh
+++ iptables.sh
@@ -1,14 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 echo "configuring iptables firewall.."
 
 # Delete all existing rules.
 sudo iptables -F
 
-# Allow established, loopback, and ssh traffic.
+# Allow established traffic.
 sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
+# Allow loopback traffic from localhost.
 sudo iptables -A INPUT -i lo -j ACCEPT
-sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+sudo iptables -A INPUT ! -i lo -s 127.0.0.0/8 -j REJECT
+
+# Allow SSH traffic from local network.
+sudo iptables -A INPUT -s 192.168.0.0/16 -p tcp --dport 22 -j ACCEPT
 
 # Drop all other incoming traffic.
 sudo iptables -A INPUT -j DROP
